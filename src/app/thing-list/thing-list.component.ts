a64dc12c911ac96fe3ad7f64eb2e585afa68d8d6
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-31 10:31:17 -0300

    Generate thing list component files

---
+++ src/app/thing-list/thing-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'thing-list',
+  templateUrl: './thing-list.component.html',
+  styleUrls: ['./thing-list.component.scss']
+})
+export class ThingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
