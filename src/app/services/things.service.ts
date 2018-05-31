ce8d948b96248f229580de100a8001a18777794a
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-31 14:36:21 -0300

    implement things service

--- src/app/services/things.service.ts
+++ src/app/services/things.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { Thing } from '../thing-list/thing.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ThingsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getThings(): Observable<Thing[]> {
+    return this.http.get<Thing[]>('assets/things.json');
+  }
 }
