0cae3c8c1fef7b8e74faf508b96469488af43671
Guilherme dos Reis <greis.gnr@gmail.com>
2018-05-31 14:37:30 -0300

    implement basic structure and logic for thing list component

--- src/app/thing-list/thing-list.component.ts
+++ src/app/thing-list/thing-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ThingsService } from '../services/things.service';
+import { Thing } from './thing.model';
+
 @Component({
   selector: 'thing-list',
   templateUrl: './thing-list.component.html',
   styleUrls: ['./thing-list.component.scss']
 })
 export class ThingListComponent implements OnInit {
+  things: Thing[];
 
-  constructor() { }
+  constructor(private thingsService: ThingsService) { }
 
   ngOnInit() {
+    this.thingsService.getThings()
+      .subscribe(things => this.things = things);
+  }
+
+  rate(thing) {
+    console.log('thing: ', thing);
   }
 
 }
