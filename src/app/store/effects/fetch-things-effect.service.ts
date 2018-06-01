a652e767656829ee75d0cc6a0cd5d41837945331
Guilherme dos Reis <greis.gnr@gmail.com>
2018-06-01 20:16:56 -0300

    Implement fetch things effect service

--- src/app/store/effects/fetch-things-effect.service.ts
+++ src/app/store/effects/fetch-things-effect.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Actions, Effect } from '@ngrx/effects';
+import { Observable, pipe } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
+
+import { FETCH_THINGS_ACTION, FetchThingsAction, ThingsFetchedAction } from '../actions';
+import { ThingsService } from '../../services/things.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchThingsEffectService {
 
-  constructor() { }
+  constructor(private actions$: Actions, private thingsService: ThingsService) { }
+
+  @Effect() things$: Observable<Action> = this.actions$
+    .ofType<FetchThingsAction>(FETCH_THINGS_ACTION)
+    .pipe(
+      switchMap(action => this.thingsService.getThings())
+    )
+    .pipe(
+      map(things => new ThingsFetchedAction(things))
+    );
+
 }
