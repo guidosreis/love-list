19ff936f934046a3c4061b6e79a0f8a1ed4af79b
Guilherme dos Reis <greis.gnr@gmail.com>
2018-06-01 21:38:43 -0300

    Implement rate thing effect service

--- src/app/store/effects/rate-thing-effect.service.ts
+++ src/app/store/effects/rate-thing-effect.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Actions, Effect } from '@ngrx/effects';
+
+import { THING_RATED_ACTION, ThingRatedAction, SortThingsAction } from '../actions';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RateThingEffectService {
 
-  constructor() { }
+  constructor(private actions$: Actions) { }
+
+  @Effect() things$: Observable<Action> = this.actions$
+    .ofType<ThingRatedAction>(THING_RATED_ACTION)
+    .pipe(
+      map(() => new SortThingsAction())
+    );
 }
