1023a22c22fba86cdc318af9423dee2f2657f41b
Guilherme dos Reis <greis.gnr@gmail.com>
2018-06-04 08:17:37 -0300

    Update fetch things effect service to use just one pipe

--- src/app/store/effects/fetch-things-effect.service.ts
+++ src/app/store/effects/fetch-things-effect.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Actions, Effect } from '@ngrx/effects';
-import { Observable, pipe } from 'rxjs';
+import { Observable } from 'rxjs';
 import { switchMap, map } from 'rxjs/operators';
 
 import { FETCH_THINGS_ACTION, FetchThingsAction, ThingsFetchedAction } from '../actions';
@@ -17,9 +17,7 @@ export class FetchThingsEffectService {
   @Effect() things$: Observable<Action> = this.actions$
     .ofType<FetchThingsAction>(FETCH_THINGS_ACTION)
     .pipe(
-      switchMap(action => this.thingsService.getThings())
-    )
-    .pipe(
+      switchMap(action => this.thingsService.getThings()),
       map(things => new ThingsFetchedAction(things))
     );
 
