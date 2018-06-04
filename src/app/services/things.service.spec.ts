2fcd39c3a4eec9fdf8216508fcb918818c8a0723
Guilherme dos Reis <greis.gnr@gmail.com>
2018-06-04 19:14:47 -0300

    Write tests for things service

--- src/app/services/things.service.spec.ts
+++ src/app/services/things.service.spec.ts
@@ -1,15 +1,52 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { ThingsService } from './things.service';
+import { Thing } from '../thing-section/thing.model';
 
 describe('ThingsService', () => {
+  let service: ThingsService;
+  let httpMock: HttpTestingController;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [ThingsService]
     });
+
+    httpMock = TestBed.get(HttpTestingController);
+    service = TestBed.get(ThingsService);
   });
 
-  it('should be created', inject([ThingsService], (service: ThingsService) => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  fit('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  fdescribe('getThings', () => {
+    fit('should return an Observable<Thing[]>', () => {
+      const mock: Thing[] = [
+        {
+          name: 'Javascript',
+          rating: 0
+        },
+        {
+          name: 'Angular',
+          rating: 0
+        }
+      ];
+
+      service.getThings().subscribe(things => {
+        expect(things.length).toBe(2);
+        expect(things).toEqual(mock);
+      });
+
+      const req = httpMock.expectOne('assets/things.json');
+      expect(req.request.method).toBe('GET');
+      req.flush(mock);
+    });
+  });
 });
