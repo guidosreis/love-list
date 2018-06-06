d25d9c434a4b3029d9c9439ce8127187af193ea8
Guilherme dos Reis <greis.gnr@gmail.com>
2018-06-06 01:14:44 -0300

    Import and use mocked thing list in reducer test file

--- src/app/store/reducer.spec.ts
+++ src/app/store/reducer.spec.ts
@@ -2,28 +2,16 @@ import * as fromApplicationState from './application-state';
 import * as fromReducer from './reducer';
 import * as fromActions from './actions';
 import { sortThings } from './sortThings';
-import { Thing } from '../thing-section/thing.model';
+import { things } from '../thing-section/things.mock';
 
 describe('Reducer', () => {
   let INITIAL_APPLICATION_STATE: fromApplicationState.ApplicationState;
-  let mock: Thing[];
   let validIndex: number;
   let invalidIndex: number;
 
   beforeEach(() => {
     INITIAL_APPLICATION_STATE = fromApplicationState.INITIAL_APPLICATION_STATE;
-    mock = [
-      {
-        name: 'Javascript',
-        rating: 0
-      },
-      {
-        name: 'Angular',
-        rating: 0
-      }
-    ];
-
-    validIndex = Math.floor(Math.random() * mock.length + 0);
+    validIndex = Math.floor(Math.random() * things.length + 0);
     invalidIndex = 99999;
   });
 
@@ -38,24 +26,24 @@ describe('Reducer', () => {
 
   describe('THINGS_FETCHED action', () => {
     it('should populate things list', () => {
-      const action = new fromActions.ThingsFetchedAction(mock);
+      const action = new fromActions.ThingsFetchedAction(things);
       const state = fromReducer.reducer(INITIAL_APPLICATION_STATE, action);
 
-      expect(state.things).toEqual(mock);
+      expect(state.things).toEqual(things);
     });
   });
 
   describe('THING_RATED action', () => {
     it('should return a clone of original state when can not find the thing', () => {
       const action = new fromActions.ThingRatedAction(invalidIndex);
-      const state = fromReducer.reducer({things: mock}, action);
+      const state = fromReducer.reducer({things: things}, action);
 
-      expect(state.things).toEqual(mock);
+      expect(state.things).toEqual(things);
     });
 
     it('should increase the rating of thing given its index', () => {
       const action = new fromActions.ThingRatedAction(validIndex);
-      const state = fromReducer.reducer({things: mock}, action);
+      const state = fromReducer.reducer({things: things}, action);
       const thing = state.things[validIndex];
 
       expect(thing.rating).toBe(1);
@@ -64,9 +52,9 @@ describe('Reducer', () => {
 
   describe('THING_ADDED action', () => {
     it('should increase the rating when thing already exists', () => {
-      const payload = mock[validIndex].name;
+      const payload = things[validIndex].name;
       const action = new fromActions.ThingAddedAction(payload);
-      const state = fromReducer.reducer({things: mock}, action);
+      const state = fromReducer.reducer({things: things}, action);
       const thing = state.things[validIndex];
 
       expect(thing.rating).toBe(1);
@@ -87,16 +75,16 @@ describe('Reducer', () => {
   describe('THING_REMOVE action', () => {
     it('should return a clone of original state when can not find the thing', () => {
       const action = new fromActions.ThingRatedAction(invalidIndex);
-      const state = fromReducer.reducer({things: mock}, action);
+      const state = fromReducer.reducer({things: things}, action);
 
-      expect(state.things).toEqual(mock);
+      expect(state.things).toEqual(things);
     });
 
     it ('should remove a item from list given its index', () => {
       const action = new fromActions.ThingRemovedAction(validIndex);
-      const state = fromReducer.reducer({things: mock}, action);
+      const state = fromReducer.reducer({things: things}, action);
 
-      expect(state.things).toEqual(mock);
+      expect(state.things).toEqual(things);
     });
   });
 
@@ -104,17 +92,17 @@ describe('Reducer', () => {
     const action = new fromActions.SortThingsAction();
 
     it('should sort by name if two or more things have same rating value', () => {
-      const store = fromReducer.reducer({things: mock}, action);
+      const store = fromReducer.reducer({things: things}, action);
 
-      expect(store.things).toEqual(mock.sort(sortThings));
+      expect(store.things).toEqual(things.sort(sortThings));
     });
 
     it('should sort by the highest rated item to the lowest rated item', () => {
-      mock[validIndex].rating = 1;
+      things[validIndex].rating = 1;
 
-      const store = fromReducer.reducer({things: mock}, action);
+      const store = fromReducer.reducer({things: things}, action);
 
-      expect(store.things).toEqual(mock);
+      expect(store.things).toEqual(things);
     });
   });
 });
