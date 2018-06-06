6b937c41d2bf151d2925ae5c2ff3b38d251b6772
Guilherme dos Reis <greis.gnr@gmail.com>
2018-06-06 01:00:59 -0300

    Create a mock of thing list

---
+++ src/app/thing-section/things.mock.ts
@@ -0,0 +1,44 @@
+import { Thing } from './thing.model';
+
+export const things: Thing[] = [
+  {
+    name: 'Javascrit',
+    rating: 0
+  },
+  {
+    name: 'One Piece',
+    rating: 0
+  },
+  {
+    name: 'Angular',
+    rating: 0
+  },
+  {
+    name: 'Star Wars',
+    rating: 0
+  },
+  {
+    name: 'Batman',
+    rating: 0
+  },
+  {
+    name: 'Tattoo',
+    rating: 0
+  },
+  {
+    name: 'Beer',
+    rating: 0
+  },
+  {
+    name: 'CSS',
+    rating: 0
+  },
+  {
+    name: 'Metallica',
+    rating: 0
+  },
+  {
+    name: 'HTML',
+    rating: 0
+  }
+];
